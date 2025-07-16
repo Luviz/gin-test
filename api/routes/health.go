a0b122dbfb82e41ab2640c8fa2091ca7b9b69889
Bardia Jedi <bardia.jedi@xenit.se>
2025-07-16 11:22:08 +0200

    something

--- api/routes/health.go
+++ api/routes/health.go
@@ -1,11 +1,18 @@
 package routes
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/url"
+
+	"github.com/gin-gonic/gin"
+)
 
 func HealthRoute(group gin.RouterGroup) gin.RouterGroup {
+	myUrl, _ := url.Parse("http://myhost.local/ep/")
+	myUrl.Path += "/test/api"
 	group.GET("health", func(ctx *gin.Context) {
 		ctx.JSONP(202, gin.H{
-			"status": "OK",
+			"status":  "OK",
+			"baseUrl": myUrl.String(),
 		})
 	})
 
